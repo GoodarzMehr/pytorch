689af6ebc927ccf6b0310efd95d71adedabd499f
GoodarzMehr <goodarzm@vt.edu>
2025-04-30 13:11:33 -0400

    Fix SparseCUDABlas

--- aten/src/ATen/native/sparse/cuda/SparseCUDABlas.cpp
+++ aten/src/ATen/native/sparse/cuda/SparseCUDABlas.cpp
@@ -158,7 +158,7 @@ void _csrmm2(
     beta,
     descC,
     cusparse_value_type,  /* data type in which the computation is executed */
-    CUSPARSE_CSRMM_ALG1,  /* default computing algorithm for CSR sparse matrix format */
+    CUSPARSE_SPMM_CSR_ALG1,  /* default computing algorithm for CSR sparse matrix format */
     &bufferSize           /* output */
   ));
 
@@ -172,7 +172,7 @@ void _csrmm2(
     beta,
     descC,
     cusparse_value_type,  /* data type in which the computation is executed */
-    CUSPARSE_CSRMM_ALG1,  /* default computing algorithm for CSR sparse matrix format */
+    CUSPARSE_SPMM_CSR_ALG1,  /* default computing algorithm for CSR sparse matrix format */
     dataPtr.get()         /* external buffer */
   ));
 
